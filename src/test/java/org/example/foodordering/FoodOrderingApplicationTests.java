32ee28bafde227dcc2c02e7fbb0132093b60d397
Yovel <yovel.d.31.4@mountblue.tech>
2025-03-27 09:33:22 +0530

    Initial commit

---
+++ src/test/java/org/example/foodordering/FoodOrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.foodordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
