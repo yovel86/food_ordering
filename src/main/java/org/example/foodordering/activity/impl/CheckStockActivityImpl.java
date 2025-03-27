96c03398c9de5462ed710c86f805b2dde97c3768
rishikesh <rishiborate24@gmail.com>
2025-03-27 11:21:16 +0530

    add CheckStockActivityImpl

---
+++ src/main/java/org/example/foodordering/activity/impl/CheckStockActivityImpl.java
@@ -0,0 +1,27 @@
+package org.example.foodordering.activity.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.foodorderingsample.activity.CheckStockActivity;
+import org.example.foodorderingsample.repository.FoodItemRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CheckStockActivityImpl implements CheckStockActivity {
+
+  private final FoodItemRepository foodItemRepository;
+
+  @Override
+  public boolean checkStock(List<Long> itemIds) {
+    log.info("Checking the stock for item IDs: {}", itemIds);
+    List<Long> unavailableItems = itemIds.stream()
+            .filter(itemId -> !foodItemRepository.existsByIdAndIsAvailableTrue(itemId))
+            .toList();
+    return unavailableItems.isEmpty();
+  }
+
+}
