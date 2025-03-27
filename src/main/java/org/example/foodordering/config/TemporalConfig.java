28c8930fb49e34e587682ab16e85539d44830e73
saikumarreddybellam <saik123r@gmail.com>
2025-03-27 09:52:01 +0530

    Add TemporalConfig

---
+++ src/main/java/org/example/foodordering/config/TemporalConfig.java
@@ -0,0 +1,17 @@
+package org.example.foodordering.config;
+
+import io.temporal.client.WorkflowClient;
+import io.temporal.serviceclient.WorkflowServiceStubs;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TemporalConfig {
+
+  @Bean
+  public WorkflowClient workflowClient() {
+    WorkflowServiceStubs service = WorkflowServiceStubs.newLocalServiceStubs();
+    return WorkflowClient.newInstance(service);
+  }
+
+}
